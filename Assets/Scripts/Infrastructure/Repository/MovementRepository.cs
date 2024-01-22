51fbeaeed090d3e90e205287fd54545c44f7e371
leonardoGasperin <leonardovdg@hotmail.com>
2024-01-22 18:39:55 -0300

    do movimentan on X axis

---
+++ Assets/Scripts/Infrastructure/Repository/MovementRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Repository;
+using UnityEngine;
+
+namespace Infracstructure.Repository
+{
+    public class MovementRepository : IMovementRepository
+    {
+        public Vector2 MovementOnXAxis(Vector2 transformPosition, float speed, int direction)
+        {
+            transformPosition.x += (speed * direction) * Time.deltaTime;
+            return transformPosition;
+        }
+    }
+}
