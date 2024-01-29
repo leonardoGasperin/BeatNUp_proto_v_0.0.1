2677b68122583a21d4e3ba6e2c66748d4d9d9b1f
leonardoGasperin <leonardovdg@hotmail.com>
2024-01-29 19:25:29 -0300

    criando regras para perseguição

--- Assets/Scripts/Domain/Rules/CombatRules.cs
+++ Assets/Scripts/Domain/Rules/CombatRules.cs
@@ -4,10 +4,20 @@ namespace Domain.Rules
 {
     public static class CombatRules
     {
-        public static bool CanDoDamage(int targetLayer, int characterLayer, bool isAttacking)
+        public static bool CanDoDamage(int targetLayer, int characterLayer, bool isAttacking) => 
+            characterLayer != targetLayer && targetLayer == LayerMask.NameToLayer("Player")
+            || targetLayer == LayerMask.NameToLayer("Enemy") && isAttacking;
+
+        public static bool CanSeePlayer(Transform targetPosition, Transform selfPosition)
         {
-            return characterLayer != targetLayer && targetLayer == LayerMask.NameToLayer("Player")
-                || targetLayer == LayerMask.NameToLayer("Enemy") && isAttacking;
+            LayerMask targetLayer = targetPosition.gameObject.layer;
+            int layerMask = 1 << targetLayer;
+            Vector2 directionToPlayer = targetPosition.position - selfPosition.position;
+            Vector2 drawEnd = (Vector2)selfPosition.position + directionToPlayer.normalized * 5f;
+            RaycastHit2D hit = Physics2D.Raycast(selfPosition.position, directionToPlayer, 5f, layerMask);
+            Debug.DrawLine(selfPosition.position, drawEnd, Color.red);
+
+            return hit.collider != null && hit.collider.gameObject.layer == targetLayer;
         }
     }
 }
