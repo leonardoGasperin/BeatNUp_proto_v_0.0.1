65afcfbdffee32343a0801e99b8ec2d6ff26f6a2
leonardoGasperin <leonardovdg@hotmail.com>
2024-01-29 17:18:11 -0300

    inimigo tem a capacidade de ver jogador a uma determinada distancia

--- Assets/Scripts/Domain/Entities/Enemy.cs
+++ Assets/Scripts/Domain/Entities/Enemy.cs
@@ -1,5 +1,6 @@
 using Domain.Enum;
 using Domain.Primitive;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 namespace Domain.Entities
@@ -7,32 +8,27 @@ namespace Domain.Entities
     public class Enemy : Character
     {
         private SubjectA subjectA;
-        private Transform transform;
         public EnemyType enemyType;
 
         protected override void Start()
         {
             base.Start();
-            transform = GetComponent<Transform>();
             subjectA = GameObject.FindGameObjectWithTag("Player").GetComponent<SubjectA>();
         }
 
         protected override void Update()
         {
             base.Update();
+        }
 
+        private bool CanSeePlayer()
+        {
             Vector2 directionToPlayer = subjectA.transform.position - transform.position;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, Mathf.Infinity, subjectA.gameObject.layer);
+            Debug.DrawLine(transform.position, (Vector2)transform.position + directionToPlayer.normalized * 5f, Color.red);
+            int layerMask = 1 << subjectA.gameObject.layer;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, 5f, layerMask);
 
-            if (hit.collider != null && hit.collider.gameObject == subjectA)
-            {
-                Debug.Log("O inimigo pode ver o jogador");
-            }
-            else
-            {
-                Debug.Log("O inimigo não pode ver o jogador");
-            }
-
+            return hit.collider != null && hit.collider.gameObject.layer == subjectA.gameObject.layer;
         }
     }
 }
