d4afe510911ae6b45fadcc66509ca13737cb2526
leonardoGasperin <leonardovdg@hotmail.com>
2024-01-30 00:17:54 -0300

    melhorando outras rules e criado rule para desingage

--- Assets/Scripts/Domain/Rules/CombatRules.cs
+++ Assets/Scripts/Domain/Rules/CombatRules.cs
@@ -5,8 +5,12 @@ namespace Domain.Rules
     public static class CombatRules
     {
         public static bool CanDoDamage(int targetLayer, int characterLayer, bool isAttacking) =>
-            characterLayer != targetLayer && targetLayer == LayerMask.NameToLayer("Player")
-            || targetLayer == LayerMask.NameToLayer("Enemy") && isAttacking;
+            characterLayer != targetLayer
+            && (
+                targetLayer == LayerMask.NameToLayer("Player")
+                || targetLayer == LayerMask.NameToLayer("Enemy")
+            )
+            && isAttacking;
 
         public static bool CanSeePlayer(Transform targetPosition, Transform selfPosition)
         {
@@ -20,14 +24,17 @@ namespace Domain.Rules
                 5f,
                 layerMask
             );
-            Debug.DrawLine(selfPosition.position, drawEnd, Color.red);
+            Debug.DrawLine(
+                selfPosition.position + new Vector3(0, 0.1f),
+                drawEnd + new Vector2(0, 0.1f),
+                Color.red
+            );
 
             return hit.collider != null && hit.collider.gameObject.layer == targetLayer;
         }
 
-        public static bool CanHitPlayer(Transform targetPosition, Transform selfPosition)
+        public static bool CanHitPlayer(LayerMask targetLayer, Transform selfPosition)
         {
-            LayerMask targetLayer = targetPosition.gameObject.layer;
             int layerMask = 1 << targetLayer;
             RaycastHit2D hit = Physics2D.Raycast(
                 selfPosition.position,
@@ -35,9 +42,40 @@ namespace Domain.Rules
                 1f,
                 layerMask
             );
-            Debug.DrawLine(selfPosition.position, selfPosition.position + selfPosition.right, Color.blue);
-            
+            Debug.DrawLine(
+                selfPosition.position,
+                selfPosition.position + selfPosition.right,
+                Color.blue
+            );
+
             return hit.collider != null && hit.collider.gameObject.layer == targetLayer;
+        }
+
+        public static bool IsStillDesingagePlayer(
+            Transform targetPosition,
+            Transform selfPosition,
+            bool isDesingage
+        )
+        {
+            LayerMask targetLayer = targetPosition.gameObject.layer;
+            int layerMask = 1 << targetLayer;
+            Vector2 directionToPlayer = targetPosition.position - selfPosition.position;
+            RaycastHit2D hit = Physics2D.Raycast(
+                selfPosition.position,
+                directionToPlayer,
+                2.5f,
+                layerMask
+            );
+            Vector2 drawEnd = (Vector2)selfPosition.position + directionToPlayer.normalized * 2.5f;
+            Debug.DrawLine(
+                selfPosition.position + new Vector3(0, -0.1f),
+                drawEnd + new Vector2(0, -0.1f),
+                Color.yellow
+            );
+
+            return hit.collider != null
+                && isDesingage
+                && hit.collider.gameObject.layer == targetLayer;
         }
     }
 }
