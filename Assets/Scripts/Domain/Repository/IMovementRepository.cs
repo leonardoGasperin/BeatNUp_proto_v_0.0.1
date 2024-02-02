f7265dbb02d6f18d7100b05d3523499344005cc0
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-02 16:01:33 -0300

    adiciona metodo para pulo para serviço de movimento

--- Assets/Scripts/Domain/Repository/IMovementRepository.cs
+++ Assets/Scripts/Domain/Repository/IMovementRepository.cs
@@ -5,5 +5,6 @@ namespace Domain.Repository
     public interface IMovementRepository
     {
         public Vector2 MovementOnXAxis(Vector2 transformPosition, float speed, int direction);
+        public void Jump(Rigidbody2D rigidbody2D, Vector2 objectPosition, float force);
     }
 }
