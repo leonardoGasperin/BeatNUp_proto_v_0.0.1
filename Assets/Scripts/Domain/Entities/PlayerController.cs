c904d61903c7b7c6bde20486ff9b19f7d1edb61e
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-03 15:59:33 -0300

    adicionado player controller + movimentação e pulo

---
+++ Assets/Scripts/Domain/Entities/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Domain.Entities
+{
+    public sealed class PlayerController : MonoBehaviour
+    {
+        public SubjectA subjectA;
+
+        private void Start()
+        {
+            subjectA = gameObject.GetComponent<SubjectA>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetButton("Horizontal"))
+            {
+                transform.position = subjectA.movement.MovementOnXAxis(transform.position, subjectA.movementSpeed, (int)Input.GetAxisRaw("Horizontal"));
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Jump") && subjectA.canJump && subjectA.isGrounded)
+            {
+                subjectA.movement.Jump(subjectA.rigbody2D, transform.position, subjectA.jumpForce);
+            }
+        }
+    }
+}
