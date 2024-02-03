77f7c8cbc90e8f578a5f65b3bca896c66cbc20da
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-03 17:37:45 -0300

    recuperando alvo para jogador

--- Assets/Scripts/Infrastructure/Repository/CombatRepository.cs
+++ Assets/Scripts/Infrastructure/Repository/CombatRepository.cs
@@ -1,4 +1,6 @@
+using Domain.Primitive;
 using Domain.Repository;
+using UnityEngine;
 
 namespace Infracstructure.Repository
 {
@@ -7,6 +9,24 @@ namespace Infracstructure.Repository
         public int TakeDamage(int healthPoint, int dmg)
         {
             return healthPoint - dmg;
+        }
+
+        public Character GetPlayerEnemyTarget(LayerMask targetLayer, Transform selfPosition)
+        {
+            int layerMask = 1 << targetLayer;
+            RaycastHit2D hit = Physics2D.Raycast(
+                selfPosition.position,
+                selfPosition.right,
+                1f,
+                layerMask
+            );
+
+            if(hit.collider != null)
+            {
+                return hit.collider.gameObject.GetComponent<Character>();
+            }
+
+            return null;
         }
     }
 }
