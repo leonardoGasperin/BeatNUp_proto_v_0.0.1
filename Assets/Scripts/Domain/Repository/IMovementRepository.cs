886a3ddc63b4b74f154f853c18e8f8eb02915e1a
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-05 13:29:51 -0300

    refatorando MovementOnXAxis()

--- Assets/Scripts/Domain/Repository/IMovementRepository.cs
+++ Assets/Scripts/Domain/Repository/IMovementRepository.cs
@@ -4,7 +4,7 @@ namespace Domain.Repository
 {
     public interface IMovementRepository
     {
-        public Vector2 MovementOnXAxis(Vector2 transformPosition, float speed, int direction);
+        public void MovementOnXAxis(Transform transformPosition, float speed, int direction);
         public void Jump(Rigidbody2D rigidbody2D, Vector2 objectPosition, float force);
     }
 }
