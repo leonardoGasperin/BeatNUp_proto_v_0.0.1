3cbcff8304f5e5a3a354a8ad1664150fb527f8db
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-05 16:33:29 -0300

    criado classe para criação de raycast2D

---
+++ Assets/Scripts/Infrastructure/Misc/RayCastUtillity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.Misc
+{
+    public static class RayCastUtillity
+    {
+        public static RaycastHit2D GetHit(Transform selfPosition, Vector2 rayDirection, float raySize, int targetLayer)
+        {
+            return Physics2D.Raycast(
+                selfPosition.position,
+                rayDirection,
+                raySize,
+                targetLayer
+            );
+        }
+
+        public static void DebugGetHitRaycast(Vector2 selfPosition, Vector2 rayDirection, float raySize, float rayPosition, Color color)
+        {
+            Vector2 drawEnd = selfPosition + rayDirection.normalized * raySize;
+            Debug.DrawLine(
+                selfPosition + new Vector2(0, rayPosition),
+                drawEnd + new Vector2(0, rayPosition),
+                color
+            );
+        }
+    }
+}
